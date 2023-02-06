1cb181451e2b07fb98feab0a6b1c48f7053fe62c
arpitsha25 <arpitsharma1234x@gmail.com>
2023-02-06 13:33:42 +0530

    updated

--- src/Components/3_Slider_Events/getevent.jsx
+++ src/Components/3_Slider_Events/getevent.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllEvents } from "../../Redux/Actions/eventAction";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import Loader from "../loader/loader"
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -15,7 +15,7 @@ import Eventcard from "./Event_cards/eventcard";
 
 const Getevents = () => {
   const dispatch = useDispatch();
-  const { error, eventdetails } = useSelector((state) => state.event);
+  const { error, eventdetails, loading } = useSelector((state) => state.event);
   useEffect(() => {
     dispatch(getAllEvents());
   }, [dispatch]);
@@ -28,7 +28,8 @@ const Getevents = () => {
 
   // console.log(eventdetails?.eventdata);
   // {console.log(eventdetails?.eventdata[0].imagesID[0])}
-  // leng = eventdetails?.eventdata.length;
+  // leng = eventdetails?.eventdata.length;4
+  console.log("loading", loading);
   return (
     <div>
       {/* <h1>{eventdetails?.eventdata[0]?.name}</h1> */}
@@ -42,41 +43,46 @@ const Getevents = () => {
       </div>
       {/* <img src="/getimage/63ae80a254127b761371f534" alt="myImage" /> */}
       <ToastContainer position="top-center" theme="dark" />
-      <Swiper
-        // slidesPerView={3}
-        breakpoints={{
-          640: { slidesPerView: 1 },
-          845: { slidesPerView: 2 },
-          1275: { slidesPerView: 3 },
-        }}
-        spaceBetween={10}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={false}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="eventswiper"
-      >
-        {eventdetails?.eventdata?.map((e, index) => {
-          if (index < 10) {
-            return (
-              <SwiperSlide className="eventslide">
-                {/* {console.log("index",index)} */}
-                {/* {console.log(eventdetails?.eventdata[0])} */}
-                <Eventcard
-                  carddata={eventdetails?.eventdata[index]}
-                  images={eventdetails?.eventdata[index].imagesID[0]}
-                />
-              </SwiperSlide>
-            );
-          }
-          return <></>;
-        })}
-      </Swiper>
+
+      {loading ? (
+        <div className="shwloder"><Loader/></div>
+      ) : (
+        <Swiper
+          // slidesPerView={3}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            845: { slidesPerView: 2 },
+            1275: { slidesPerView: 3 },
+          }}
+          spaceBetween={10}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={false}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="eventswiper"
+        >
+          {eventdetails?.eventdata?.map((e, index) => {
+            if (index < 10) {
+              return (
+                <SwiperSlide className="eventslide">
+                  {/* {console.log("index",index)} */}
+                  {/* {console.log(eventdetails?.eventdata[0])} */}
+                  <Eventcard
+                    carddata={eventdetails?.eventdata[index]}
+                    images={eventdetails?.eventdata[index].imagesID[0]}
+                  />
+                </SwiperSlide>
+              );
+            }
+            return <></>;
+          })}
+        </Swiper>
+      )}
     </div>
   );
 };
